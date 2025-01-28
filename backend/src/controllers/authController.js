8746c39baf7ce26fb98aa9b3f6f96f2b05a1fac1
AnojAryal <anoj1810@gmail.com>
2025-01-28 15:34:37 +0545

    Refresh tokens added

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -25,6 +25,15 @@ const handleUserLogin = async (req, res) => {
       { expiresIn: "1d" }
     );
 
+    const refreshToken = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_REFRESH_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    // Save refresh token to user
+    user.refreshToken = refreshToken;
+
     //set token in the Authorization header
     res.setHeader("Authorization", `Bearer ${token}`);
 
@@ -32,13 +41,46 @@ const handleUserLogin = async (req, res) => {
     res.status(200).json({
       message: "Login successful",
       user: { id: user._id, username: user.username },
-      accessToken : token
+      accessToken: token,
+      refreshToken: refreshToken,
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
+//generate refresh token
+const handleRefreshToken = async (req, res) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    return res.status(401).json({ error: "Refresh token is required" });
+  }
+
+  const user = await User.findOne({ refreshToken });
+
+  if (!user) {
+    return res.status(403).json({ error: "Invalid refresh token" });
+  }
+
+  // Verify the refresh token
+  jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid refresh token" });
+    }
+
+    // If the refresh token is valid, create a new access token
+    const newAccessToken = jwt.sign(
+      { id: decoded.id, username: decoded.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({ accessToken: newAccessToken });
+  });
+};
+
 module.exports = {
   handleUserLogin,
+  handleRefreshToken,
 };
--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
-const { handleUserLogin } = require("../controllers/authController");
+const {
+  handleUserLogin,
+  handleRefreshToken,
+} = require("../controllers/authController");
 
 const router = express.Router();
 
 //login
 router.post("/", handleUserLogin);
+
+// Refresh token route
+router.post("/refresh-token", handleRefreshToken);
 
 module.exports = router;
